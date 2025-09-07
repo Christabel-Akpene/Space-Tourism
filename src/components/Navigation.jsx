7c0cc229a104adb9d7bb501d6321f25c4db05496
Christabel-Akpene <dc.akpene@gmail.com>
2025-09-07 23:57:22 +0000

    feat: add navigation page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,21 @@
 
 const Navigation = () => {
   return (
-    <div className="bg-red-400">
+    <div className="py-10 flex justify-between items-center text-white">
+        <div className="pl-20">
+            <img src="images/shared/logo.svg" alt="Logo" />
+        </div>
+
+        <div className="bg-slate-700/35 flex items-center py-8">
+            <ul className="flex space-x-11 text-sm pl-50 pr-20">
+                <li className="font-thin"><span className="font-extrabold text-lg pr-2">00</span>  <span className="font-light ">HOME</span></li>
+                <li className="font-thin"><span className="font-extrabold text-lg pr-2">01</span> DESTINATION</li>
+                <li className="font-thin"><span className="font-extrabold text-lg pr-2">02</span> CREW</li>
+                <li className="font-thin"><span className="font-extrabold text-lg pr-2">03</span> TECHNOLOGY</li>
+            </ul>
+        </div>
+
+
 
     </div>
   )
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,22 @@
 import Navigation from "../components/Navigation"
-import { Outlet } from "react-router"
+import { Outlet, useLocation } from "react-router"
 
 const Layout = () => {
+    const location = useLocation();
+
+    console.log(location.pathname);
+    const backgroundImages = {
+        "/": "images/home/background-home-desktop.jpg",
+        "/crew": "images/crew/background-crew-desktop.jpg",
+        "/destination": "images/destination/background-destination-desktop.jpg",
+        "/technology": "images/technology/background-technology-desktop.jpg",
+    }
+
+    const backgroundImage = backgroundImages[location.pathname] || "none";
+
+
   return (
-    <div>
+    <div style={{backgroundImage: `url(${backgroundImage})`}} className="font-cardo bg-cover bg-center bg-fixed min-h-dvh">
         <Navigation/>
         <Outlet/>
     </div>
