fbb97b529672ed6f171a9796cdedcb2316adc644
Christabel-Akpene <dc.akpene@gmail.com>
2025-09-17 06:37:23 +0000

    style:add responsive design for technology and crew

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -8,19 +8,19 @@ const Crew = () => {
   };
 
   return (
-    <div className="text-white w-[80vw] m-auto">
-      <p className="text-lg">
+    <div className="text-white w-full px-10 lg:px-0 lg:w-[80vw] m-auto">
+      <p className="text-sm sm:mt-10 sm:text-lg lg:mt-0">
         <span className="text-gray-500 mr-4 font-bold">02</span>PICK YOUR CREW
       </p>
 
-      <div className="flex space-x-28 justify-between items-center mt-10 pb-10">
-        <div className="w-[35vw]">
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-28 justify-between items-center mt-10 pb-10">
+        <div className="text-center lg:text-left lg:w-[35vw]">
           <p className="text-2xl mb-8 uppercase">{data.crew[selected].role}</p>
-          <p className="mb-8 text-4xl font-semi-bold">
+          <p className="mb-8 text-4xl font-semi-bold sm:text-6xl">
             {data.crew[selected].name}
           </p>
-          <p className="font-serif ">{data.crew[selected].bio}</p>
-            <div className="flex space-x-6 flex-row">
+          <p className="font-serif sm:w-3/4 lg:w-full m-auto ">{data.crew[selected].bio}</p>
+            <div className="flex justify-center lg:justify-start space-x-6 flex-row">
               {data.crew.map((person, index) => {
                 return (
                   <span
--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -9,13 +9,13 @@ const Technology = () => {
   };
 
   return (
-    <div className="text-white ml-32">
-      <p className="text-lg ">
+    <div className="text-white lg:ml-32">
+      <p className="text-sm text-center sm:text-left sm:ml-8 lg:ml-0 sm:mt-10 sm:text-lg mb-18 lg:mb-0 lg:mt-0">
         <span className="text-gray-500 mr-4 font-bold">03</span>SPACE LAUNCH 101
       </p>
 
-      <div className="flex justify-between items-center space-x-10 pb-20">
-        <div className="flex flex-col">
+      <div className="flex flex-col lg:flex-row justify-between items-center space-x-10 pb-20">
+        <div className="flex m-auto lg:m-0  lg:flex-col space-x-5 my-10 lg:my-0 order-2 lg:order-1">
               {data.technology.map((technology, index) => {
                 return (
                   <button
@@ -25,23 +25,25 @@ const Technology = () => {
                       selected === index ? " bg-white text-black" : "border border-white"
                     } w-14 h-14 text-2xl text-bold mt-8 flex items-center justify-center text-center rounded-full cursor-pointer`}
                   >
-                    
                     {index + 1}
                   </button>
                 );
               })}
         </div>
 
-        <div className="w-2/6"> 
+        <div className="w-full m-auto px-10 text-center lg:text-left lg:w-2/6 order-3 lg:order-2"> 
           <p className="text-2xl mb-5 text-gray-500">THE TERMINOLOGY...</p>
           <p className="mb-8 text-4xl font-semi-bold">
             {data.technology[selected].name}
           </p>
-          <p className="font-serif ">{data.technology[selected].description}</p>
+          <p className="font-serif lg:w-full">{data.technology[selected].description}</p>
         </div>
 
-        <div className="">
-          <img src={data.technology[selected].images.portrait}   className="max-w-md w-full h-auto object-contain" />
+        <div className="order-1 lg:order-3">
+          <picture>
+            <source srcset={data.technology[selected].images.portrait} media="(min-width:1024px)" />
+            <img src={data.technology[selected].images.landscape} alt="image"  className="w-full h-auto object-contain lg:max-w-md "  />
+          </picture>
         </div>
 
       </div>
